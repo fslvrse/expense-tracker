e37646da52520516ceffada791399a25b78bdbce
faisal <fslvrse2004@gmail.com>
2025-04-14 20:17:10 +0300

    Deploying the latest build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/fslvrse/", // Replace 'fslvrse' with your actual repository name
+});
